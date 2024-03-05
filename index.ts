eb3490e64ac514a78e7d465337f18da3e9b569e6
Your Name <shlokjagtap123@gmail.com>
2024-03-05 16:09:14 +0530

    prisma done

--- index.ts
+++ index.ts
@@ -4,10 +4,40 @@ const prisma = new PrismaClient();
 
 async function main() {
     try {
-        const getAllUsers = await prisma.article.findMany({
-           
+        // //create user and article and association 
+        // const user = await prisma.user.create({
+        //     data: {
+        //         name: "LMAO",
+        //         email: "LMAO@gmail.com",
+        //         articles: {
+        //             create: {
+        //                     title: "LAMOS FIRST ARTICLE",
+        //                     body:'body of LAMOS'
+                            
+        //                 }
+        //             }
+        //         }
+                
+        //     }
+        // )
+
+        const createOneMore = await prisma.article.create({
+            data: {
+                title: "this is extra title",
+                body: "this is one more extra body",
+                author: {
+                    connect: {
+                        id:2,
+                    }
+                }
+            }
         })
-        console.log(getAllUsers)
+console.log(createOneMore)
+        const users = await prisma.user.findMany({
+            include: {
+            articles:true,
+        }})
+       console.log(users)
     } catch (error) {
         console.error(error);
     } finally {
